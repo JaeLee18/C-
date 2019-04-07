66ab5472a753d4102b707f557e46650a8aed1aac
JaeLee18 <lee2161@purdue.edu>
2019-04-08 08:12:24 +0900

    added vector

--- Chap3/chap3.cpp
+++ Chap3/chap3.cpp
@@ -2,6 +2,7 @@
 #include <ios>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cin; using std::setprecision;
 using std::cout; using std::string;
@@ -24,9 +25,26 @@ int main(){
 	double sum = 0;
 
 	double x;
+	// >> returns "cin" in this case, if there are no inputs then
+	// it returns False.
+	/*
+		"cin" returns False when
+		1. EOF
+		2. No type-matched
+		3. System detects any malfuncitons from input hardwares.
+	*/
+
+	/*
+	Using while statement, no way to calculate median, so use Vector
 	while (cin >> x){
 		count += 1;
 		sum += x;
+	}
+	*/
+
+	std::vector<double> homework;
+	while(cin >> x){
+		homework.push_back(x);
 	}
 
 	// Get the current precison level
