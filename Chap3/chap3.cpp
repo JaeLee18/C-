71a8e1e1ef886755c79fd797d3a691418af53d9b
JaeLee18 <lee2161@purdue.edu>
2019-04-08 08:18:15 +0900

    Using vector, get Median value

--- Chap3/chap3.cpp
+++ Chap3/chap3.cpp
@@ -3,10 +3,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using std::cin; using std::setprecision;
 using std::cout; using std::string;
 using std::endl; using std::streamsize;
+using std::sort; using std::vector;
 
 int main(){
 	cout << "Your name is : ";
@@ -47,10 +49,24 @@ int main(){
 		homework.push_back(x);
 	}
 
+	typedef vector<double>::size_type vec_sz;
+	vec_sz size = homework.size();
+	if (size == 0){
+		cout << endl << "Must endter at least one grade. " << endl;
+		return 1;
+	}
+	sort(homework.begin(), homework.end());
+
+	vec_sz mid = size / 2;
+	double median;
+
+	median = size % 2 == 0 ? (homework[mid] + homework[mid - 1]) / 2 : homework[mid];
+
+
 	// Get the current precison level
 	// setprecision(prec): resotre to the current precision level
 	streamsize prec = cout.precision();
 	cout << "Your final grade is " << setprecision(3)
-	<< 0.2 * midterm + 0.4 * final + 0.4 * sum / count
+	<< 0.2 * midterm + 0.4 * final + 0.4 * median
 	<< setprecision(prec) << endl;
 }
