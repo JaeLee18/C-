c632b0bd483523ea97e0f370f20fcdf2f4bc2c7e
JaeLee18 <lee2161@purdue.edu>
2019-04-09 19:25:25 +0900

    Chap4, call by value

---
+++ Chap4/chap4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+// parameter lists
+double grade(double midterm, double final, double homework){
+	return 0.2 * midterm + 0.4 * final + 0.4 * homework;
+}
+
